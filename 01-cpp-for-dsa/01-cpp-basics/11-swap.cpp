b217ff6f6f2eccad63d97486a2a26fb1a538a44b
mdsifathossainr <mdsifathossainr@gmail.com>
2026-05-15 14:29:48 +0600

    feat: add C++ basics files

---
+++ 01-cpp-for-dsa/01-cpp-basics/11-swap.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    // int temp = a;
+    // a = b;
+    // b = temp;
+
+    swap(a, b);
+
+    cout << a << " " << b << endl;
+
+    return 0;
+}
--- 01-cpp-for-dsa/01-cpp-basics/13-bits-header-file.cpp
+++ 01-cpp-for-dsa/01-cpp-basics/13-bits-header-file.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    cout << "HELLO" << endl;
+    
     return 0;
 }
